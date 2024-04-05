c0091538e52381f503990e2f067d42c008e3b745
Andranium <andranik_galstyan@bk.ru>
2024-04-05 16:54:49 +0300

    storybook fix

--- src/components/CartButton/CartButton.stories.ts
+++ src/components/CartButton/CartButton.stories.ts
@@ -4,13 +4,9 @@ import CartButton from './CartButton.tsx';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof CartButton> = {
-  title: 'Example/CartButton',
+  title: 'CartButton',
   component: CartButton,
   tags: ['autodocs'],
-  argTypes: {
-    count: 0,
-    disabled: false
-  },
 };
 
 export default meta;
