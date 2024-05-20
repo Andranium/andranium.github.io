ecd9cd8342aa60f7b5e79355304308d0fdc29fb5
Andranium <andranik_galstyan@bk.ru>
2024-05-20 11:51:37 +0300

    Color scheme change logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Modal from 'src/components/Modal/Modal';
 import './App.css';
 import Header from 'src/components/Header/Header';
 
+export const ThemeContext = createContext(null);
+
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const providerData = {
+    state: theme,
+    changeTheme(value: string) {
+      setTheme(value)
+    }
+  }
+
   return (
-    <>
+    <ThemeContext.Provider value={providerData}>
       <Header />
       <Modal shown />
-    </>
+    </ThemeContext.Provider>
   );
 }
 
