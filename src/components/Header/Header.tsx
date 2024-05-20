27de8f2b8ea75c0a6ca3bdfbd8a30ec187a939d3
Andranium <andranik_galstyan@bk.ru>
2024-05-20 11:51:37 +0300

    Color scheme change logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import styles from './Header.module.scss';
 import Logo from '../Logo/Logo';
+import { ThemeContext } from 'src/App';
 
 export default function Header() {
+  const theme = useContext(ThemeContext);
+
+  function changeTheme() {
+    const value = theme.state === 'light' ? 'dark' : 'light';
+
+    theme.changeTheme(value)
+  }
+
   return (
     <header className={styles.header}>
       <Logo />
+
+      { theme.state }
+
+      <button onClick={changeTheme}>switcher</button>
     </header>
   );
 }
