6528f9bebb43d9a3ad1dc72ffb3bfd4a6598e6ce
Andranium <andranik_galstyan@bk.ru>
2024-05-22 11:06:36 +0300

    I18n, language switcher

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,45 @@ import React, { createContext, useState } from 'react';
 import Modal from './components/Modal/Modal';
 import Header from './components/Header/Header';
 import styles from './App.scss';
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import ruLang from '../lang/ru.json';
+import enLang from '../lang/en.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enLang,
+      },
+
+      ru: {
+        translation: ruLang
+      }
+    },
+    lng: "ru",
+    fallbackLng: "ru",
+
+    interpolation: {
+      escapeValue: false
+    }
+  } as any);
 
 export const ThemeContext = createContext(null);
 
 function App() {
+  const { t, i18n } = useTranslation();
   const [theme, setTheme] = useState('light');
 
   const providerData = {
     state: theme,
+    lang: 'ru',
+    changeLang(lang: string) {
+      console.log(lang);
+
+      i18n.changeLanguage(lang);
+    },
     changeTheme(value: string) {
       setTheme(value)
     }
@@ -20,6 +51,8 @@ function App() {
       <main className={styles[`theme-${providerData.state}`]}>
         <Header />
         <Modal />
+
+        {t('my-app')}
       </main>
     </ThemeContext.Provider>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,14 @@ import React, { useContext } from 'react';
 import styles from './Header.module.scss';
 import Logo from '../Logo/Logo';
 import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
+import LangSwitch from '../LangSwitch/LangSwitch';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <Logo />
+
+      <LangSwitch />
 
       <ThemeSwitcher />
     </header>
