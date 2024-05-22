69a0f1be1e7085e1b0d3d5a51de4bc3eb90737ed
Andranium <andranik_galstyan@bk.ru>
2024-05-22 11:06:36 +0300

    I18n, language switcher

---
+++ src/components/LangSwitch/LangSwitch.tsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import styles from './LangSwitch.module.scss';
+import { ThemeContext } from '../../App';
+
+export default function LangSwitch() {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <select
+      name="lang"
+      className={styles.langSwitch}
+      onChange={e => theme.changeLang(e.target.value)}
+    >
+      <option
+        value="ru"
+      >Ru</option>
+
+      <option
+        value="en"
+      >En</option>
+    </select>
+  )
+}
